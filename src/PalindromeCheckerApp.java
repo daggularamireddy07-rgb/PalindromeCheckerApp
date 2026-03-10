e06e4d25273275a299fbd96cce6199d6992d963c
daggularamireddy07-rgb <daggularamireddy0@gmail.com>
2026-03-10 09:53:24 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,20 @@
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App");
-        System.out.println("Application Version: 1.0");
 
-    }
-}
+                String word = "madam";
+                String reverse = "";
+
+                for(int i = word.length() - 1; i >= 0; i--) {
+                    reverse = reverse + word.charAt(i);
+                }
+
+                if(word.equals(reverse)) {
+                    System.out.println(word + " is a Palindrome");
+                } else {
+                    System.out.println(word + " is not a Palindrome");
+                }
+
+            }
+        }
+
