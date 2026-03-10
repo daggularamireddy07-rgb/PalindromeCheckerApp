f63fcc203a5d050e2d62dc6133b2f0ce575f4d6f
daggularamireddy07-rgb <daggularamireddy0@gmail.com>
2026-03-10 10:50:21 +0530

    US12:Strategy Pattern for Palindrome Algorithms

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,16 +1,41 @@
-class PalindromeChecker {
+import java.util.*;
 
-    public boolean checkPalindrome(String word) {
+interface PalindromeStrategy {
+    boolean isPalindrome(String word);
+}
 
-        int start = 0;
-        int end = word.length() - 1;
+class StackStrategy implements PalindromeStrategy {
 
-        while (start < end) {
-            if (word.charAt(start) != word.charAt(end)) {
+    public boolean isPalindrome(String word) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
+        }
+
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != stack.pop()) {
                 return false;
             }
-            start++;
-            end--;
+        }
+
+        return true;
+    }
+}
+
+class DequeStrategy implements PalindromeStrategy {
+
+    public boolean isPalindrome(String word) {
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            deque.addLast(word.charAt(i));
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
         }
 
         return true;
@@ -23,9 +48,11 @@ public class PalindromeCheckerApp {
 
         String word = "madam";
 
-        PalindromeChecker checker = new PalindromeChecker();
+        PalindromeStrategy strategy;
 
-        if (checker.checkPalindrome(word)) {
+        strategy = new StackStrategy();
+
+        if (strategy.isPalindrome(word)) {
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is not a Palindrome");
